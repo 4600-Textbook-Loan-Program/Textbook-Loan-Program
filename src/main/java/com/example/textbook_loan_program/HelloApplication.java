32a7988964a0fa49bfdc58d7619aaf2fed2e5ade
DustinWZook <dustin.w.zook@gmail.com>
2025-02-21 16:34:27 -0600

    testing push

--- src/main/java/com/example/textbook_loan_program/HelloApplication.java
+++ src/main/java/com/example/textbook_loan_program/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 // thia is basic hello app
+// testing a push
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
