8eddecf4a0f146f09c17439c8d33f9340387d8f3
DustinWZook <dustin.w.zook@gmail.com>
2025-04-01 11:42:39 -0500

    connected database and added classes associated with

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.textbook_loan_program {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.textbook_loan_program to javafx.fxml;
-    exports com.example.textbook_loan_program;
-}
